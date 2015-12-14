a5e09f68b8660bf9da8e046143552c04430f06e8
Dominik Jakobowicz <dominikjakobowicz@gmail.com>
2015-12-14 21:22:12 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<html>
+ <head>
+  <title>testy z VPS'em</title>
+ </head>
+ <body>
+ <form action="" method="POST">
+ <input type="text" name="imie">
+ <input type="submit" name="go" value="wyslij">
+ </form>
+
+ </body>
+ </html>
+
 <?php
  $imie = "dominik";
   if (imie == "dominik"){
